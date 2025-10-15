e905a3f7bae208510691c37c7f8a69df4d17372c
if1t <rostislav@mokuton.ru>
2025-10-15 11:12:22 +0800

    chore/register-table: Экспорт директивы для переопределения шаблона ячейки в таблице.

--- projects/ngx-register-base/src/lib/components/register-table/index.ts
+++ projects/ngx-register-base/src/lib/components/register-table/index.ts
@@ -1,2 +1,3 @@
-export { RegisterTableComponent } from './register-table.component';
+export { CellTemplateDirective } from './directives/cell-template.directive';
 export * from './model/schema';
+export { RegisterTableComponent } from './register-table.component';
