7eb094cdfe6d2fc429237c91441315a82e544625
if1t <rostislav@mokuton.ru>
2025-10-15 14:52:21 +0800

    fix/register-table: Экспорт input-компонентов.

--- projects/ngx-register-base/src/lib/components/index.ts
+++ projects/ngx-register-base/src/lib/components/index.ts
@@ -1,5 +1,6 @@
 export { FilterButtonComponent } from './filter-button/filter-button.component';
-export * from './register-table/index';
 export * from './filters/index';
+export * from './inputs/index';
+export * from './register-table/index';
 export * from './sliding-panel/sliding-panel.module';
 export { SlidingPanelComponent } from './sliding-panel/sliding-panel.component';
