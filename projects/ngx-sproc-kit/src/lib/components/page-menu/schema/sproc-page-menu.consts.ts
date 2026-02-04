0b6f6537a142fc4685ce508b2ee2aab6f94d65a1
if1t <rostislav@mokuton.ru>
2026-02-04 13:39:25 +0700

    refactor/rename: Переименование библиотеки.

---
+++ projects/ngx-sproc-kit/src/lib/components/page-menu/schema/sproc-page-menu.consts.ts
@@ -0,0 +1,8 @@
+export const SIDE_MENU_OPENED_WIDTH = 350;
+export const SIDE_MENU_CLOSED_WIDTH = 48;
+
+export enum EPageMenuIconsSrc {
+  CHEVRON_RIGHT = 'assets/ngx-sproc-kit/icons/chevron-right.svg',
+  CHEVRON_UP = 'assets/ngx-sproc-kit/icons/chevron-up.svg',
+  MENU_CHEVRON = 'assets/ngx-sproc-kit/icons/chevron-menu.svg',
+}
