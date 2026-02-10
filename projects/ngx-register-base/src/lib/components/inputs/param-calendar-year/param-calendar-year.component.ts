2e3007b923798fd87d342fe8bb984e30050acf7b
if1t <rostislav@mokuton.ru>
2026-02-10 09:28:03 +0700

    feature/updates: Обновление param-calendar-year.

--- projects/ngx-register-base/src/lib/components/inputs/param-calendar-year/param-calendar-year.component.ts
+++ projects/ngx-register-base/src/lib/components/inputs/param-calendar-year/param-calendar-year.component.ts
@@ -1,7 +1,15 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  inject,
+  input,
+  viewChild,
+} from '@angular/core';
 import { type TuiBooleanHandler } from '@taiga-ui/cdk';
 import { TuiSizeL, TuiSizeM } from '@taiga-ui/core';
 import { ParamBase } from '../../../core/param/param-base';
+import { TuiInputYearComponent } from '@taiga-ui/legacy';
 
 @Component({
   selector: 'sproc-param-calendar-year',
@@ -10,6 +18,9 @@ import { ParamBase } from '../../../core/param/param-base';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ParamCalendarYearComponent extends ParamBase<number, string> {
+  private readonly _cdr = inject(ChangeDetectorRef);
+  public yearInputComponent = viewChild<TuiInputYearComponent>('yearInput');
+
   override buildShowedValue = input((value: number): string => value?.toString() ?? '-');
   protected readonly disabledHandler: TuiBooleanHandler<number> = (value) =>
     this.disabledYears().includes(value);
@@ -18,4 +29,24 @@ export class ParamCalendarYearComponent extends ParamBase<number, string> {
   public min = input<number | null>(null);
   public max = input<number | null>(null);
   public size = input<TuiSizeL | TuiSizeM>('m');
+  protected isYearPickerOpen = false;
+
+  protected click(): void {
+    this.isYearPickerOpen = !this.isYearPickerOpen;
+
+    const yearComponent = this.yearInputComponent();
+    if (yearComponent) {
+      const inputEl = yearComponent.nativeFocusableElement!;
+      const onDocumentClick = (event: MouseEvent) => {
+        if (!inputEl.contains(event.target as Node) && this.isYearPickerOpen) {
+          (yearComponent as any).onOpenChange(false);
+          yearComponent.checkControlUpdate();
+          document.removeEventListener('click', onDocumentClick);
+          this.isYearPickerOpen = false;
+          this._cdr.markForCheck();
+        }
+      };
+      document.addEventListener('click', onDocumentClick);
+    }
+  }
 }
