346d44beac153ab22cc3d540caa4edc2775689cc
if1t <rostislav@mokuton.ru>
2026-02-11 10:24:42 +0700

    feature/updates: Обновление paginator.

--- projects/ngx-register-base/src/lib/directives/number/number-only.directive.ts
+++ projects/ngx-register-base/src/lib/directives/number/number-only.directive.ts
@@ -1,17 +1,36 @@
-import { Directive, HostListener, ElementRef, inject } from '@angular/core';
+import { Directive, HostListener, input } from '@angular/core';
+import { InputElementEvent } from '../../types';
 
+/** Ввод только числовых выражений */
 @Directive({
   standalone: true,
   selector: '[numberOnly]',
 })
 export class NumberOnlyDirective {
-  private el = inject(ElementRef);
+  /** Разрешать пользователю вводить дробные числа (возможность использовать точку или запятую) */
+  public allowFractional = input(false);
 
   @HostListener('input', ['$event'])
-  onInput(event: Event): void {
-    // eslint-disable-next-line xss/no-mixed-html
-    const input = event.target as HTMLInputElement;
-    input.value = input.value.replace(/\D/g, '');
-    this.el.nativeElement.value = input.value;
+  public onInput(event: InputElementEvent): void {
+    const element = event.target;
+    let value = element.value;
+
+    if (this.allowFractional()) {
+      value = value.replace(/,/g, '.');
+
+      value = value.replace(/[^\d.]/g, '');
+
+      // Проверяем, чтобы точка была только одна
+      const parts: string[] = value.split('.');
+      if (parts.length > 2) {
+        value = `${parts.shift()!}.${parts.join('')}`;
+      }
+    } else {
+      value = value.replace(/\D/g, '');
+    }
+
+    if (element.value !== value) {
+      element.value = value;
+    }
   }
 }
