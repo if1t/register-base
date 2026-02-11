e372cae87bef7f1ca76899b7c76fae98cd5fa4c4
if1t <rostislav@mokuton.ru>
2026-02-11 10:32:54 +0700

    feature/updates: Обновление sliding-panel.

--- projects/ngx-register-base/src/lib/components/sliding-panel/sliding-panel.component.ts
+++ projects/ngx-register-base/src/lib/components/sliding-panel/sliding-panel.component.ts
@@ -1,29 +1,36 @@
-import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostBinding, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'sproc-sliding-panel',
   templateUrl: './sliding-panel.component.html',
   styleUrls: ['./sliding-panel.component.less'],
 })
-export class SlidingPanelComponent implements AfterViewInit {
-  /** Состояние видимости панели */
-  @Input() isOpen: boolean | null | undefined = false;
+export class SlidingPanelComponent {
+  @HostBinding('style.--header-offset-height')
+  get headerOffsetHeightPx(): string {
+    const height: number = this._headerContainer?.nativeElement.children[0]?.offsetHeight ?? 0;
+
+    return `${height}px`;
+  }
+
+  @HostBinding('style.--footer-offset-height')
+  get footerOffsetHeightPx(): string {
+    const height: number = this._footerContainer?.nativeElement.children[0]?.offsetHeight ?? 0;
+
+    return `${height}px`;
+  }
+
+  @HostBinding('style.--sub-offset-height')
+  get subOffsetHeightPx(): string {
+    return `${this.subOffsetHeight}px`;
+  }
+
   /** Доп. отступ для вычисления высоты панели (в пикселях) */
   @Input() subOffsetHeight = 0;
 
-  @ViewChild('headerContainer') private readonly _headerContainer!: ElementRef;
-  @ViewChild('footerContainer') private readonly _footerContainer!: ElementRef;
+  /** Состояние видимости панели */
+  @Input() isOpen: boolean | null | undefined = false;
 
-  protected footerOffsetHeight = 0;
-
-  private _headerOffsetHeight = 0;
-
-  public ngAfterViewInit(): void {
-    this._headerOffsetHeight = this._headerContainer.nativeElement.children[0]?.offsetHeight ?? 0;
-    this.footerOffsetHeight = this._footerContainer.nativeElement.children[0]?.offsetHeight ?? 0;
-  }
-
-  protected get scrollbarHeight(): string {
-    return `calc(100vh - var(--header-height) - ${this.subOffsetHeight}px - ${this._headerOffsetHeight}px)`;
-  }
+  @ViewChild('headerContainer') private readonly _headerContainer: ElementRef | undefined;
+  @ViewChild('footerContainer') private readonly _footerContainer: ElementRef | undefined;
 }
