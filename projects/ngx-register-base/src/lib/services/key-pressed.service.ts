2f2294f342b5932dc1c700de63a8a10f48bcef01
if1t <rostislav@mokuton.ru>
2026-02-12 09:42:35 +0700

    feature/updates: Обновление key-pressed.service.

--- projects/ngx-register-base/src/lib/services/key-pressed.service.ts
+++ projects/ngx-register-base/src/lib/services/key-pressed.service.ts
@@ -1,10 +1,10 @@
-import { inject, Injectable, InjectionToken } from '@angular/core';
+import { inject, Injectable, InjectionToken, OnDestroy } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 export const KEY_PRESSED = new InjectionToken<string>('KEY_PRESSED_TOKEN');
 
 @Injectable()
-export class KeyPressedService {
+export class KeyPressedService implements OnDestroy {
   private _keyName = inject(KEY_PRESSED);
 
   private readonly _isPressed$ = new BehaviorSubject(false);
@@ -12,19 +12,29 @@ export class KeyPressedService {
   public readonly isPressed$ = this._isPressed$.asObservable();
 
   constructor() {
-    window.addEventListener('keydown', (event) => {
-      if (event.key === this._keyName) {
-        this._isPressed$.next(true);
-      }
-    });
-    window.addEventListener('keyup', (event) => {
-      if (event.key === this._keyName) {
-        this._isPressed$.next(false);
-      }
-    });
+    window.addEventListener('keydown', this._keydownListener);
+    window.addEventListener('keyup', this._keyupListener);
   }
 
   public isPressed(): boolean {
     return this._isPressed$.getValue();
   }
+
+  public ngOnDestroy(): void {
+    this._isPressed$.complete();
+    window.removeEventListener('keydown', this._keydownListener);
+    window.removeEventListener('keyup', this._keyupListener);
+  }
+
+  private readonly _keydownListener = (event: KeyboardEvent) => {
+    if (event.key === this._keyName) {
+      this._isPressed$.next(true);
+    }
+  };
+
+  private readonly _keyupListener = (event: KeyboardEvent) => {
+    if (event.key === this._keyName) {
+      this._isPressed$.next(false);
+    }
+  };
 }
