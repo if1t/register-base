edc426871e022d09789d77ef60b843eb30a7c266
if1t <rostislav@mokuton.ru>
2026-05-29 10:49:39 +0700

    feature(filters): Обновление компонентов панели фильтров.

--- projects/ngx-register-base/src/lib/components/filters/components/filter-list/filter-edit/filter-edit.component.ts
+++ projects/ngx-register-base/src/lib/components/filters/components/filter-list/filter-edit/filter-edit.component.ts
@@ -1,31 +1,31 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { FiltersStateService } from '../../../../../services';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { TuiTextfield } from '@taiga-ui/core';
 
 @Component({
   selector: 'sproc-filter-edit',
+  standalone: true,
   templateUrl: './filter-edit.component.html',
   styleUrls: ['./filter-edit.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [ReactiveFormsModule, TuiTextfield],
 })
-export class FilterEditComponent implements OnInit, OnDestroy {
-  public readonly valueControl: FormControl<string | null> = new FormControl<string | null>('');
-  private subscription: Subscription | null = null;
+export class FilterEditComponent implements OnInit {
+  private readonly _filterStateService = inject(FiltersStateService);
+  private readonly _dr = inject(DestroyRef);
 
-  constructor(private readonly _filterStateService: FiltersStateService<any>) {}
+  protected readonly filterNameControl = new FormControl<string>('', {
+    nonNullable: true,
+  });
 
   public ngOnInit(): void {
-    const name = this._filterStateService.getSelectedSavedFilter()?.name ?? '';
-
-    this.valueControl.setValue(name);
-    this._filterStateService.setName(name);
-    this.subscription = this.valueControl.valueChanges.subscribe((value) => {
-      this._filterStateService.setName(value ?? '');
+    this.filterNameControl.valueChanges.pipe(takeUntilDestroyed(this._dr)).subscribe((value) => {
+      this._filterStateService.setName(value);
     });
-  }
 
-  public ngOnDestroy(): void {
-    this.subscription?.unsubscribe();
+    const name = this._filterStateService.getSelectedSavedFilter()?.name ?? '';
+    this.filterNameControl.setValue(name);
   }
 }
